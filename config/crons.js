886c4aa2cdbf1c0260d6c4d073ad15c9a2ddc4f4
Greg Bergé <neoziro@gmail.com>
2013-03-21 02:41:25 +0100

    first mail at 10h

--- config/crons.js
+++ config/crons.js
@@ -3,5 +3,5 @@ require('./application');
 var PROCESS_DIR = global.base + '/app/processes',
 cron = require('cron').CronJob;
 
-exports.poll = new cron('0 11 * * *', require(PROCESS_DIR + '/poll-email'));
+exports.poll = new cron('0 10 * * *', require(PROCESS_DIR + '/poll-email'));
 exports.result = new cron('0 12 * * *', require(PROCESS_DIR + '/result-email'));
